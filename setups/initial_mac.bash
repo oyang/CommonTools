8aa20c08baf0b20360bac1a6703f8e369449b145
Oliver Yang <oyang@sugarcrm.com>
2016-04-28 21:31:54 -0700

    Adding intitial script for Mac OS X

---
+++ setups/initial_mac.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+BASH_IT="$HOME/.bash_it"
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # no color
+Warning="${YELLOW}Warning:${NC}"
+Error="${RED}Red:${NC}"
+
+# install homebrew first http://brew.sh/
+function install_brew() {
+  if ! brew -v >/dev/null 2>&1 ; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    echo -e "${Warning} Homebrew already be installed!"
+  fi
+}
+
+# install wget, curl, git
+function install_brew_package() {
+  local install_list=(git wget curl the_silver_searcher fzf)
+
+  for i in ${install_list[@]}; do
+    brew install $i
+  done
+}
+
+# install Bash-it https://github.com/Bash-it/bash-it
+function install_bashit() {
+  if ! [ -d "${BASH_IT}" ] ; then
+      git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+      source ~/.bash_it/install.sh
+  else
+    echo -e "${Warning} Bash-it already be installed!"
+  fi
+}
+
+# enable bashit package
+function install_bashit_package() {
+  # fasd plugin make you move fast between directory
+  source $BASH_IT/bash_it.sh
+  bash-it enable plugin fasd
+
+  # to make the enabled plugin take effect
+  source ~/.bashrc
+}
+
+function main() {
+  install_brew
+  install_brew_package
+
+  install_bashit
+  install_bashit_package
+}
+
+main

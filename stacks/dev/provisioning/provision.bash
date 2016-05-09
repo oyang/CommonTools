c46ca13d3a3b5b20a0f2fad33d18027559bf1c4a
Oliver Yang <oyang@sugarcrm.com>
2016-05-09 15:52:19 -0700

    Adding more packages

--- stacks/dev/provisioning/provision.bash
+++ stacks/dev/provisioning/provision.bash
@@ -1,32 +1,68 @@
 #!/bin/bash
-function install_builtin_rpm() {
+RUBY_VER=2.3.0
+
+function config_yum() {
+  echo "Config yum"
+  sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm >/dev/null
+  sudo rpm -Uvh http://dev.mysql.com/get/mysql57-community-release-el6-7.noarch.rpm >/dev/null
+
+  for item in $(ls provisioning/yum) ; do
+    sudo cp provisioning/yum/$item /etc/yum.repos.d
+  done
+  echo "Done"
+}
+
+function install_rpm() {
   echo "Installing built-in dependencies"
-  package_group='Development tools'
+  local package_group='Development tools'
+  local packages=()
 
-  packages[0]='vim'
-  packages[1]='unzip'
-  packages[2]='autoconf213'
-  packages[3]='libxml2-devel'
-  packages[4]='apr-devel'
-  packages[5]='openssl-devel'
-  packages[6]='libcurl-devel'
-  packages[7]='gd-devel'
-  packages[8]='libc-client-devel'
-  packages[9]='openldap-devel'
-  packages[10]='httpd-devel'
-  packages[11]='mysql-devel'
-  packages[12]='httpd'
-  packages[13]='mysql-server'
-  packages[14]='xz-devel'
+  # Base utility
+  packages+=("vim")
+  packages+=("unzip")
+  packages+=("bash-completion")
+  packages+=("the_silver_searcher")
 
-  sudo yum groupinstall "${package_group}"
-  sudo yum install -y "${packages[@]}"
+  # Devel dependencies for PHP
+  packages+=("autoconf213")
+  packages+=("libxml2-devel")
+  packages+=("apr-devel")
+  packages+=("openssl-devel")
+  packages+=("libcurl-devel")
+  packages+=("gd-devel")
+  packages+=("libc-client-devel")
+  packages+=("openldap-devel")
+  packages+=("httpd-devel")
+  packages+=("xz-devel")
+
+  # Http server
+  packages+=("httpd")
+
+  # MySQL
+  packages+=("mysql-community-server")
+  packages+=("mysql-community-client")
+  packages+=("mysql-community-devel")
+
+  # RVM and Ruby dependencies
+  pacakges+=("patch")
+  pacakges+=("libyaml-devel")
+  pacakges+=("glibc-headers")
+  pacakges+=("glibc-devel")
+  pacakges+=("readline-devel")
+  pacakges+=("libffi-devel")
+  pacakges+=("libtool")
+  pacakges+=("bison")
+  pacakges+=("sqlite-devel")
+
+  # Finally install all the packages
+  sudo yum groupinstall "${package_group}" >/dev/null
+  sudo yum install -y "${packages[@]}" >/dev/null
   echo "Done"
 }
 
 function link_autoconf() {
   echo "Linking autoconf213"
-  
+
   if [ ! -f /usr/bin/autoconf ]; then
     sudo ln -s /usr/bin/autoconf-2.13 /usr/bin/autoconf
   fi
@@ -47,19 +83,35 @@ function remove_welcome_conf() {
 
 function install_rvm() {
   echo "Installing RVM to select right Ruby"
+
+  # Download the GPG key first
+  gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
   if ! rvm -v >/dev/null 2>&1 ; then
-    curl -sSL https://get.rvm.io | bash -s stable --ruby
+    curl -sSL https://get.rvm.io | bash -s stable --ruby >/dev/null
     source ~/.rvm/scripts/rvm
   else
     echo "RVM already existed!"
   fi
+
+  # Select the default ruby
+  rvm --default use ${RUBY_VER}
   ruby -v
+
   echo "Done"
 }
 
 function install_fpm() {
   echo "Install fpm for building portable package"
-  gem install fpm
+  gem install fpm >/dev/null
+  echo "Done"
+}
+
+function config_httpd() {
+  echo "Config httpd"
+  for item in $(ls provisioning/httpd) ; do
+    sudo cp provisioning/httpd/$item /etc/httpd/conf
+  done
   echo "Done"
 }
 
@@ -78,15 +130,17 @@ function start_mysql_server() {
 function main() {
   cd /vagrant
 
-  install_builtin_rpm
+  config_yum
+
+  install_rpm
   link_autoconf
 
   install_rvm
   install_fpm
 
   remove_welcome_conf
+  config_httpd
   reload_httpd
-  start_mysql_server
 }
 
 main

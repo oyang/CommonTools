50b5e9c9e8644ce3c6c1fe251cef9855a5747932
Oliver Yang <oyang@sugarcrm.com>
2016-05-09 23:23:33 -0700

    Adding mysql secure installation

--- stacks/php5329/provisioning/provision.bash
+++ stacks/php5329/provisioning/provision.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 DOCUMENT_ROOT='/var/www'
+MYSQL_PASSWORD='test'
 
 function remove_packages() {
   while [ -n "$1" ]; do
@@ -15,13 +16,16 @@ function install_builtin_rpm() {
   # Import PGP key first
   rpm --import http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-6
 
-  packages[0]="vim"
-  packages[1]="unzip"
-  packages[2]="httpd"
-  packages[3]="libc-client-devel"
-  packages[4]="libpng"
-  packages[5]="git-all"
-  packages[6]='centos-release'
+  local packages=()
+  packages+=("expect")
+  packages+=("vim")
+  packages+=("unzip")
+  packages+=("httpd")
+  packages+=("libc-client-devel")
+  packages+=("libpng")
+  packages+=("git-all")
+  packages+=("centos-release")
+  packages+=("libmcrypt-devel")
 
   yum install -y "${packages[@]}" >/dev/null
   echo "Done"
@@ -31,15 +35,28 @@ function install_epel() {
   echo "Installing EPEL for more packages"
   remove_packages epel-release webtatic-release
   rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm >/dev/null
-  rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm >/dev/null
+  rpm -Uvh http://dev.mysql.com/get/mysql57-community-release-el6-7.noarch.rpm >/dev/null
+  echo "Done"
+}
+
+function config_yum() {
+  echo "Configing yum repo"
+  local target_path="/etc/yum.repos.d/"
+  for item in $(ls provisioning/yum) ; do
+    cp "provisioning/yum/$item" ${target_path}
+  done
   echo "Done"
 }
 
 function install_mysql55() {
   echo "Installing MySQL55 server and client"
-  local to_remove=(mysql mysql-server mysql-libs)
+  local to_remove=()
+  to_remove+=("mysql")
+  to_remove+=("mysql-server")
+  to_remove+=("mysql-libs")
+
   remove_packages ${to_remove[@]}
-  yum install -y mysql55w mysql55w-server >/dev/null
+  yum install -y mysql-community-client mysql-community-server >/dev/null
   echo "Done"
 }
 
@@ -63,6 +80,10 @@ function copy_httpd_conf() {
   local src_path="provisioning/httpd/httpd.conf"
   local target_path="/etc/httpd/conf/httpd.conf"
 
+  if [ -f ${target_path} ];then
+    mv ${target_path} "${target_path}.bak"
+  fi
+
   cp ${src_path} ${target_path}
   echo "Done"
 }
@@ -71,6 +92,10 @@ function copy_php_ini() {
   echo "Applying php.ini"
   local src_path="provisioning/php/php.ini"
   local target_path="/usr/local/lib/php.ini"
+
+  if [ -f ${target_path} ]; then
+    mv ${target_path} "${target_path}.bak"
+  fi
 
   cp ${src_path} ${target_path}
   echo "Done"
@@ -105,6 +130,43 @@ function start_mysql_server() {
   echo "Done"
 }
 
+function mysql_secure_install() {
+  local current_passwd=""
+
+  if mysql -uroot -p"${MYSQL_PASSWORD}" -e "select now();" 2>&1 >/dev/null ; then
+    echo "Ignored! Won't run mysql_secure_installation twice!"
+    return 0
+  fi
+
+  if ! mysql -uroot -e "select now();" 2>&1 >/dev/null ; then
+    echo "You updated the default MySQL password, can not run mysql_secure_installation!"
+    return 1
+  fi
+
+  echo "Running /usr/bin/mysql_secure_installation"
+  expect <<EOD
+  log_user 0
+  spawn /usr/bin/mysql_secure_installation
+  expect "Enter current password for root"
+  send "${current_passwd}\r"
+  expect "Change the root password"
+  send "Y\r"
+  expect "New password"
+  send "${MYSQL_PASSWORD}\r"
+  expect "Re-enter new password"
+  send "${MYSQL_PASSWORD}\r"
+  expect "Remove anonymous users"
+  send "Y\r"
+  expect "Disallow root login remotely"
+  send "n\r"
+  expect "Remove test database and access to it"
+  send "Y\r"
+  expect "Reload privilege tables now"
+  send "Y\r"
+EOD
+  echo "Done."
+}
+
 function install_phpmyadmin() {
   echo "Installing phpMyAdmin"
   local phpmyadmin_source_link="https://files.phpmyadmin.net/phpMyAdmin/4.0.10.15/phpMyAdmin-4.0.10.15-all-languages.tar.gz"
@@ -129,6 +191,7 @@ function main() {
 
   install_builtin_rpm
   install_epel
+  config_yum
   install_mysql55
   install_customized_php
   copy_httpd_conf
@@ -137,6 +200,7 @@ function main() {
   remove_welcome_conf
   reload_httpd
   start_mysql_server
+  mysql_secure_install
   install_phpmyadmin
 }
 
